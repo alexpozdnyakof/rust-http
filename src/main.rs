0204190a665d56c04e4fd48118848053c8acb7d2
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-24 03:24:12 +0700

    feat: add thread pool

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
-
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream)
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
     }
 }
 
@@ -18,10 +24,14 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+
     };
 
     let contents = fs::read_to_string(filename).unwrap();
